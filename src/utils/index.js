3baf5d9fd0216dc17c5c1216874b89c91aa6cd3b
Sailesh Kumar <gsaileshkumar@gmail.com>
2022-01-13 11:32:30 +0800

    added comments

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,13 @@
+/**
+ *
+ * @param list array of objects
+ * @param keyGetter function that retrieves the key based on which the resulting array is grouped
+ * @returns grouped array.
+ * 		each array consits of 2 elements
+ * 		-> 1 is the grouped by property
+ * 		-> 2 is the array of objects belong to that group
+ */
+
 export const groupBy = (list, keyGetter) => {
 	const map = new Map();
 	list.forEach((item) => {
